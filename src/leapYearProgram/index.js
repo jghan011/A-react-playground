f529a1e119e45f3caa27c22717d9fb1e3174bdc7
Jason <jghan011@gmail.com>
2021-02-13 00:55:05 -0600

    changes to test

--- src/leapYearProgram/index.js
+++ src/leapYearProgram/index.js
@@ -1,21 +1,4 @@
-// function LeapYear (year){
 
-//     if (year != 1582){
-//         return true;
-//     }
-//     if (year % 4 || year % 400){
-//         return true;
-//     }
-//     if (year % 100) {
-//         return false
-//     }
-//     co
-
-// }
-// LeapYear();
-// console.log(LeapYear(1582));
-
-// export default LeapYear;
 
 function leapYear(year) {
     if(year < 1582) {
@@ -24,7 +7,7 @@ function leapYear(year) {
     const div400 = year % 400 === 0;
     const div100 = year % 100 === 0;
     const div4 = year % 4 === 0;
-    return div400 || (div4 || !div100);
+    return div400 && (div4 || !div100);
   }
   
   // The next line makes the function available to other JavaScript modules
