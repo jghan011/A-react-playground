7234303d1c4b3ebe556461d2c30211350157f4a9
Jason <jghan011@gmail.com>
2021-02-13 23:32:28 -0600

    changes to index in test

--- src/leapYearProgram/index.test.js
+++ src/leapYearProgram/index.test.js
@@ -8,7 +8,7 @@ it('should be a leap year if divisible by 4, not 100', () => {
   expect(leapYear(1984)).toBe(true);
 });
 
-it('should NOT be a leap year if not divisible by 4', () => {
+it('Should NOT be a leap year if divisible by 4', () => {
   expect(leapYear(1983)).toBe(false);
 });
 
@@ -20,4 +20,5 @@ it('should throw an error for years before 1582', () => {
   expect(() => {
     leapYear(1568);
   }).toThrow();
+
 });
