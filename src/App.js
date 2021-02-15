d1c1c11385360446fe87be129056d468c6a8eae8
Jason <jghan011@gmail.com>
2021-02-15 03:20:47 -0600

    Added messages component alo ng with snapshot

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Split from './composition/Split';
 import './App.css';
-import Tooltip from './composition/Tooltip'
-
-const firstTooltip = (
-  <Tooltip color="hotpink" message= 'tooltip message'>
-    ipsum
-  </Tooltip>
-
-)
-
-const secondTooltip = (
-  <Tooltip color="#126BCC" message = 'another tooltip message'>
-    officiis
-  </Tooltip>
-
-)
+// import Tooltip from './composition/Tooltip'
+import Messages from './composition/Messages';
 
 
+// const firstTooltip = (
+//   <Tooltip color="hotpink" message= 'tooltip message'>
+//     ipsum
+//   </Tooltip>
+
+// )
+
+// const secondTooltip = (
+//   <Tooltip color="#126BCC" message = 'another tooltip message'>
+//     officiis
+//   </Tooltip>
+
+// )
 
 
-function App() {
-  return (
-    <main className='App'>
-      <Split className='left' flexBasis={2}>
-      This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex {secondTooltip} velit suscipit facere officia?<br />
-        {/* make another tooltip directly inside the App */}
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>  
-      </Split>
-      <Split className="right">
-        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
-  );
+
+
+// export default function App() {
+//   return (
+//     <main className='App'>
+//       <Split className='left' flexBasis={2}>
+//       This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex {secondTooltip} velit suscipit facere officia?<br />
+//         {/* make another tooltip directly inside the App */}
+//         <Tooltip message='one more tooltip message'>
+//           Necessitatibus?
+//         </Tooltip>  
+//       </Split>
+//       <Split className="right">
+//         This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+//       </Split>
+//     </main>
+    
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className ='App'>
+        <h1>YOUR APPLICATION NAME !</h1>
+        <Messages name='Messages' unread={0}/>
+        <Messages name='Notifications' unread={10}/>
+      </div>  
+
+    );
+  }
 }
 
 export default App;
