9a84c4a9dc2b1e5d988e8a0a2b88764e256d402a
Jason <jghan011@gmail.com>
2021-02-15 18:52:41 -0600

    Changes to snapshot. created test suite

--- src/composition/Messages.test.js
+++ src/composition/Messages.test.js
@@ -9,9 +9,29 @@ import renderer from 'react-test-renderer';
 //     ReactDOM.unmountComponentAtNode(div);
 // })
 
-it('renders the UI as expected', () => {
-    const tree = renderer
-      .create(<Messages name="Messages" unread={4}/>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();  
-    });
+describe('Messages component', () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Messages />, div);
+        ReactDOM.unmountComponentAtNode(div);
+      });
+    
+    
+    it('renders the UI as expected', () => {
+        const tree = renderer
+          .create(<Messages name="Messages" unread={4}/>)
+          .toJSON();
+        expect(tree).toMatchSnapshot();  
+        });
+    
+      it('renders the UI as expected with no unreads', () => {
+         const tree = renderer
+            .create(<Messages name= "Messages" unread={0}/>)
+            .toJSON();
+            expect(tree).toMatchSnapshot();
+        })
+
+
+
+})
