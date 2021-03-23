99db8b433feb8733c84c506e4e2717fe876d0762
Jason <jghan011@gmail.com>
2021-03-22 19:39:37 -0500

    changes to snapshot and new accordion added to state drills folder

--- src/state/Tabs/Tabs.test.js
+++ src/state/Tabs/Tabs.test.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import renderer from 'react-test-renderer'
 import ReactDOM from 'react-dom';
-
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json'
 import Tabs from './Tabs';
 
-describe(`Tabs Component`, () => {
-    // array of objects, each with a name and content
-    const tabsProp = [/* ... */];
-  
-    /* smoke test not shown */
-  
-    it('renders the first tab by default', () => {
-      const tree = renderer.create(<Tabs tabs={tabsProp} />).toJSON()
-      expect(tree).toMatchSnapshot()
-    })
-  })
+describe('Tabs Component', ()=>{
+const tabsProp = [/* contents not shown*/]
+
+it('renders without errors', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Tabs />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('renders empty given no tabs', () => {
+  const wrapper = shallow(<Tabs />)
+  expect(toJson(wrapper)).toMatchSnapshot()
+})
+
+it('renders the first tab by default', () => {
+  const wrapper = shallow(<Tabs tabs={tabsProp} />)
+  expect(toJson(wrapper)).toMatchSnapshot()
+})
+
+it('closes the first tab and opens any clicked tab', () => {
+  const wrapper = shallow(<Tabs tabs={tabsProp} />)
+  wrapper.find('button').at(1).simulate('click')
+  expect(toJson(wrapper)).toMatchSnapshot()
+})
+
+})
